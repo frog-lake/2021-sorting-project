2494c86726d2726e3b505f767f38f6c0edbf981b
Evelina Prosyankina <prosyae2@l128-30.computing.dcu.ie>
2020-12-28 15:43:14 +0000

    added first part of program optimisation

--- src/main.c
+++ src/main.c
@@ -7,22 +7,29 @@
 
 void print_array(int *array, int len);
 void measure_runtime(int *array, int len, int type);
+void gen_array(int len, int type);
 
 int main(int argc, char *argv[])
 {
-	int len = 0;
-	int type;
+	// var declarations
+	int len = 0, type;
+
+	// if argc has 3 args, continue
 	if(argc == 3)
 	{
 		len = atoi(argv[1]);
 		type = atoi(argv[2]);
 	}
+	else{return 0;}
+	gen_array(len, type);
+	return 0;
+}
 
+void gen_array(int len, int type)
+{
 	int array[len];
 	gen_rand(array, len);
-	int partial[len];
-	copyarray(array, 0, len, partial);
-	partial_sort(partial, len);
+
 	int reverse[len];
 	copyarray(array, 0, len, reverse);
 	reverse_sort(reverse, len);
@@ -31,32 +38,39 @@ int main(int argc, char *argv[])
 	measure_runtime(array, len, type);
 	printf("-------\n");
 
+/*
+	int partial[len];
+	copyarray(array, 0, len, partial);
+	partial_sort(partial, len);
 	printf("partial\n");
 	measure_runtime(partial, len, type);
-	printf("-------\n");
+	free(partial);
 
+	printf("-------\n");
 	printf("reversed\n");
 	measure_runtime(partial, len, type);
-	printf("-------\n");
-	return 0;
+	printf("-------\n");*/
+	return;
 }
-
 
 void measure_runtime(int *array, int len, int type)
 {
 	clock_t start = clock();
 
-	if(type == 1){
+	switch(type)
+	{
+	case 1:
 		printf("bubblesort\n");
 		bubblesort(array, len);
-	}
-	else if(type == 2){
+		break;
+	case 2:
 		printf("quicksort\n");
 		quicksort(array, 0, len - 1);
-	}
-	else if(type == 3){
+		break;
+	case 3:
 		printf("mergesort\n");
 		mergesort(array, len);
+		break;
 	}
 	//print_array(array, len);
 	clock_t end = clock();
