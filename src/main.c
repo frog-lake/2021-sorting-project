efdcdcec53a9104253bc94e8f91a318f9f4e7408
Evelina Prosyankina <prosyae2@l128-30.computing.dcu.ie>
2020-12-28 18:29:18 +0000

    added sorted dataset + minor optimisation

--- src/main.c
+++ src/main.c
@@ -27,35 +27,39 @@ int main(int argc, char *argv[])
 
 void gen_array(int len, int type)
 {
-	int array[len];
+	int array[len], workarray[len];
 	gen_rand(array, len);
 
-	int reverse[len];
-	copyarray(array, 0, len, reverse);
-	reverse_sort(reverse, len);
-
-	printf("random\n");
+	printf("random: ");
+	copyarray(array, 0, len, workarray);
 	measure_runtime(array, len, type);
-	printf("-------\n");
+//	printf("-------\n");
 
-/*
-	int partial[len];
-	copyarray(array, 0, len, partial);
-	partial_sort(partial, len);
-	printf("partial\n");
-	measure_runtime(partial, len, type);
-	free(partial);
+	printf("partial: ");
+	copyarray(array, 0, len, workarray);
+	partial_sort(workarray, len);
+	measure_runtime(workarray, len, type);
+//	printf("-------\n");
 
-	printf("-------\n");
-	printf("reversed\n");
-	measure_runtime(partial, len, type);
-	printf("-------\n");*/
+	// reversed dataset
+	printf("reversed: ");
+	copyarray(array, 0, len, workarray);
+	reverse_sort(workarray, len);
+	measure_runtime(workarray, len, type);
+//	printf("-------\n");
+
+	printf("sorted: ");
+	measure_runtime(workarray, len, type);
+//	print_array(workarray, len);
+//	printf("-------\n");
+
 	return;
 }
 
 void measure_runtime(int *array, int len, int type)
 {
-	clock_t start = clock();
+	clock_t start, end;
+	start = clock();
 
 	switch(type)
 	{
@@ -72,11 +76,10 @@ void measure_runtime(int *array, int len, int type)
 		mergesort(array, len);
 		break;
 	}
-	//print_array(array, len);
-	clock_t end = clock();
+	end = clock();
 	
 	double seconds = ((double)(end-start))/CLOCKS_PER_SEC;
-	printf("%f\n", seconds);
+	printf("%f\n\n", seconds);
 
 }
 
