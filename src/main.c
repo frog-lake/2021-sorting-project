40956a56243af66841dc7392aca45f9e218aca99
Evelina Prosyankina <prosyae2@l128-30.computing.dcu.ie>
2020-12-30 15:45:05 +0000

    implemented malloc and added some comments to main.c

--- src/main.c
+++ src/main.c
@@ -19,39 +19,41 @@ int main(int argc, char *argv[])
 	{
 		len = atoi(argv[1]);
 		type = atoi(argv[2]);
+		gen_array(len, type);
 	}
-	else{return 0;}
-	gen_array(len, type);
 	return 0;
 }
 
 void gen_array(int len, int type)
 {
-	int array[len], workarray[len];
+	int *array, *workarray;
+	array = (int *) malloc(len * sizeof(int));
+	workarray = (int *) malloc(len * sizeof(int));
+	// generate random integers
 	gen_rand(array, len);
 
+	print_array(array, len);
+
+	// datasets are copied and then sorted with the runtime being measured
+	// random
 	printf("random: ");
 	copyarray(array, 0, len, workarray);
 	measure_runtime(array, len, type);
-//	printf("-------\n");
 
 	printf("partial: ");
 	copyarray(array, 0, len, workarray);
 	partial_sort(workarray, len);
 	measure_runtime(workarray, len, type);
-//	printf("-------\n");
 
 	// reversed dataset
 	printf("reversed: ");
 	copyarray(array, 0, len, workarray);
 	reverse_sort(workarray, len);
 	measure_runtime(workarray, len, type);
-//	printf("-------\n");
 
 	printf("sorted: ");
 	measure_runtime(workarray, len, type);
-//	print_array(workarray, len);
-//	printf("-------\n");
+	free(workarray);
 
 	return;
 }
@@ -87,8 +89,8 @@ void measure_runtime(int *array, int len, int type)
 
 void print_array(int *array, int len)
 {
-	for(int i=0;i<len;++i)
-		printf("%d\n", array[i]);
+	for(int *p = array; *p ;++p)
+		printf("%d\n", p);
 }
 
 
